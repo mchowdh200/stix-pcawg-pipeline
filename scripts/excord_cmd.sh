c45bb2e8cc14a0f7c05798662271e37d920d83c5
murad <mchowdh200@gmail.com>
2023-06-05 12:09:34 -0600

    added excord smk pipeline

--- scripts/excord_cmd.sh
+++ scripts/excord_cmd.sh
@@ -2,7 +2,6 @@
 bam=$1
 fasta=$2
 output=$3
-excord=$4
 
 # get insert-sizes of first 100k reads, to compute discordant distance
 samtools view -f66 $bam | head -100000 |
@@ -12,5 +11,5 @@ disc_dist=$(python3 scripts/get_disc_distance.py $bam.insert-sizes.txt)
 
 echo "running excord with discordant distance = $disc_dist"
 samtools view -b $bam |
-    $excord --discordantdistance $disc_dist --fasta $fasta /dev/stdin |
+    excord --discordantdistance $disc_dist --fasta $fasta /dev/stdin |
     bgzip -c > $output
