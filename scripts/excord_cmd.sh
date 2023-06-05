8c2a6891f3c956fc917fe3b06ef157501f5cac59
murad <mchowdh200@gmail.com>
2023-06-05 14:10:18 -0600

    fix excord command

--- scripts/excord_cmd.sh
+++ scripts/excord_cmd.sh
@@ -5,13 +5,7 @@ bam=$1
 fasta=$2
 output=$3
 
-# get insert-sizes of first 100k reads, to compute discordant distance
-samtools view -f66 $bam | head -100000 |
-    awk '{print sqrt($9^2)}' \
-    > $bam.insert-sizes.txt
-disc_dist=$(python3 scripts/get_disc_distance.py $bam.insert-sizes.txt)
-
 echo "running excord with discordant distance = $disc_dist"
 samtools view -b $bam |
-    excord --discordantdistance $disc_dist --fasta $fasta /dev/stdin |
+    excord --fasta $fasta /dev/stdin |
     bgzip -c > $output
