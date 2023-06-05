6214102c0e2b658d6b9a6bfce21023b9cad8bf38
murad <mchowdh200@gmail.com>
2023-06-05 13:45:51 -0600

    add excord to path

--- scripts/setup_ec2.sh
+++ scripts/setup_ec2.sh
@@ -98,6 +98,7 @@ echo 'PATH=$PATH:~/stix-pcawg-pipeline/scripts/score-client/bin' >> ~/.profile
 mkdir ~/bin
 wget -O ~/bin/excord https://github.com/brentp/excord/releases/download/v0.2.4/excord
 chmod +x ~/bin/excord
+echo "PATH=$PATH:~/bin" >> ~/.profile
 
 
 
